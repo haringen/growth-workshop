a05fc4e34a0506c6ac3dbebe4fff446d98ac100f
agibsonccc <agibson@clevercloudcomputing.com>
2014-04-26 12:05:55 -0700

    updated db and solution
    Former-commit-id: 5cf1ea25451387a3c51ae060dbbd4edd88f013bb [formerly 7b2fa22c1e73557aa2eb0557eff5260a8912e079]
    Former-commit-id: e4fbed61cd61864330900fab6209438842f2c601

--- misc/rankedbought.py
+++ misc/rankedbought.py
@@ -1,35 +1,39 @@
-import matplotlib.pyplot as plt
 from sqlalchemy import *
 import numpy as np
 from sqlalchemy.orm import sessionmaker
 from churndata import *
 from pandas import DataFrame
 from util import query_to_df
-from util import campaign_to_num,event_to_num,transform_column,hist_and_show,vectorize
 db = create_engine('sqlite:///forjar.db')
-
-
+ 
+def query_to_df(session,query):
+    """
+    Convert an sql query to a pandas data frame
+    """
+    result = session.execute(query)
+    d = DataFrame(result.fetchall())
+    d.columns = result.keys()
+    return d
+ 
 metadata = MetaData(db)
-
+ 
 Session = sessionmaker(bind=db)
-
-
+ 
+ 
 session = Session()
-
-
+ 
+ 
 """
 Counts the users by campaign id
 """
 user_dist = session.query(Users)
 user_df = query_to_df(session,user_dist)
-transform_column(user_df,'Users_Campaign_ID',campaign_to_num.get)
-
-
+ 
 
 
 q = session.query(Users.Campaign_ID,Event.Type,Users.id,Event.User_Id).filter(Event.Type == 'bought')
 d = query_to_df(session,q)
-#print d.sort('Users_id')
-grouped = d.groupby('Users_id')
-print grouped.agg({'Event_Type' : np.count_nonzero}).sort('Event_Type')
-
+grouped = d.groupby('Users_Campaign_ID')
+print grouped.groups
+#result = grouped.agg({'Event_Type' : np.count_nonzero}).sort('Event_Type')
+#print result
