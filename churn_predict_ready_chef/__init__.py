fbc21b39ceebe157043179267c7d42fada4e7c24
agibsonccc <agibson@clevercloudcomputing.com>
2014-04-28 11:14:00 -0700

    added group by example for getting visits by month
    Former-commit-id: 1e5d72f325eac3416764142c0dd96d705b1da12b [formerly cc9a857b0aab3ac458bad3c74d17fe072776a2b5]
    Former-commit-id: 17e15307cfef4917216410c9d917278c9185f0e1

--- churn_predict_ready_chef/__init__.py
+++ churn_predict_ready_chef/__init__.py
@@ -1,1 +1,1 @@
-__author__ = 'agibsonccc'
+#! /usr/lib/python
--- churn_predict_ready_chef/usertendencies.py
+++ churn_predict_ready_chef/usertendencies.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 import pandas as pd
 import numpy as np
 from pandas.tseries.index import DatetimeIndex
+from pandas import Series
 from churn_predict_ready_chef.rolling_mean import rolling_mean
 from churndata import *
 from util import query_to_df
@@ -23,12 +24,6 @@ Counts the users by campaign id
 """
 user_dist = session.query(Users).join(Visit,Users.id == Visit.user_id).add_entity(Visit)
 user_df = query_to_df(session,user_dist)
-user_ids = user_df['Users_id']
-user_df['visit_date'] = user_df['visit_date'].apply(pd.to_datetime)
-groups = user_df.groupby(['Users_id'])
-for name,group in groups:
-    print name
-    group['visit_date'] = group['visit_date'].apply(pd.to_datetime)
-    index = DatetimeIndex(group['visit_date'])
-    group['visit_date'].index = index
-    print rolling_mean(group['visit_date'],window='24h')
+print user_df.columns
+resamp = user_df.set_index('visit_date').groupby(['Users_id']).resample('M', how='sum')
+print resamp
