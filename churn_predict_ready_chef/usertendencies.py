6b3283278fd0f1ed9e0c899f2d62bd9ca8f36cb7
agibsonccc <agibson@clevercloudcomputing.com>
2014-04-28 09:47:33 -0700

    added baseline notebook for exercise on how to calculate churn features using moving averages of readychef data wrt features
    Former-commit-id: d9bba604bf5b885799809dd60056ed508a563fb5 [formerly ba03bc1693bd581dee4ca0ffc6d11122c451d177]
    Former-commit-id: 08803926844065283c0de7bba0e54ff361497b6f

--- churn_predict_ready_chef/usertendencies.py
+++ churn_predict_ready_chef/usertendencies.py
@@ -2,6 +2,7 @@ from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
 import numpy as np
+from pandas.tseries.index import DatetimeIndex
 from churn_predict_ready_chef.rolling_mean import rolling_mean
 from churndata import *
 from util import query_to_df
@@ -27,4 +28,7 @@ user_df['visit_date'] = user_df['visit_date'].apply(pd.to_datetime)
 groups = user_df.groupby(['Users_id'])
 for name,group in groups:
     print name
-    print len(group['visit_date'].index)
+    group['visit_date'] = group['visit_date'].apply(pd.to_datetime)
+    index = DatetimeIndex(group['visit_date'])
+    group['visit_date'].index = index
+    print rolling_mean(group['visit_date'],window='24h')
